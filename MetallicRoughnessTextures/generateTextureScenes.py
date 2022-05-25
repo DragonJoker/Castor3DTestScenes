3d3dead9e91ab643a86558464a811142df1514a8
DragonJoker <dragonjoker59@hotmail.com>
2022-05-25 14:10:26 +0200

    Updated test Database.

--- MetallicRoughnessTextures/generateTextureScenes.py
+++ MetallicRoughnessTextures/generateTextureScenes.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
 
-startIndex = 100
+startIndex = 1292
 
-channels = ["albedo", "roughness", "metallic", "emissive", "opacity", "occlusion", "normal", "height", "reflection", "refraction"]
+channels = ["albedo", "roughness", "metalness", "emissive", "opacity", "occlusion", "normal", "height", "reflection", "refraction"]
+channelsMasks = ["0x00ffffff", "0x00ff0000", "0x00ff0000", "0x00ffffff", "0xff000000", "0x00ff0000", "0x00ffffff", "0x00ff0000", "", ""]
 extensions = ["jpg", "jpg", "jpg", "png", "png", "jpg", "jpg", "jpg", "", ""]
-options = ["", "", "", "emissive 1.0", 'two_sided true\n			mixed_interpolation true', "", "", "parallax_occlusion one", "", "refraction_ratio 0.92"]
+options = ["",
+    "",
+    "",
+    "emissive 1.0",
+    'two_sided true\n			mixed_interpolation true',
+    "",
+    "",
+    "parallax_occlusion one",
+    "",
+    "refraction_ratio 0.92"]
 
-def writeChannel( file, channel, extension ):
+def writeChannel( file, channel, channelMask, extension ):
 	if channel != "reflection" and channel != "refraction":
 		file.write( '\n' )
 		file.write( '			texture_unit\n' )
 		file.write( '			{\n' )
-		file.write( '				channel ' + channel + '\n' )
+		file.write( '				' + channel + '_mask ' + channelMask + '\n' )
 		file.write( '				sampler "Linear"\n' )
 		file.write( '				image "Textures/Cube/' + channel + '.' + extension + '"\n' )
 		file.write( '			}\n' )
@@ -21,7 +31,7 @@ def writeChannel( file, channel, extension ):
 		file.write( '			' + channel + 's true\n' )
 
 def writeFile( file, indices ):
-	file.write( 'materials metallic_roughness\n' )
+	file.write( 'materials pbr\n' )
 	file.write( '\n' )
 	file.write( 'scene "Scene"\n' )
 	file.write( '{\n' )
@@ -50,23 +60,32 @@ def writeFile( file, indices ):
 	file.write( '		pass\n' )
 	file.write( '		{\n' )
 	file.write( '			albedo 0.75164 0.75164 0.75164 1.0\n' )
-	file.write( '			metallic 0.628281\n' )
-	file.write( '			roughness 0.8\n' )
+	if 0 in indices:
+		if 8 in indices or 9 in indices:
+			file.write( '			metalness 0.628281\n' )
+		else:
+			file.write( '			metalness 0.0\n' )
+	else:
+		file.write( '			metalness 0.628281\n' )
+	if 8 in indices or 9 in indices:
+		file.write( '			roughness 0.4\n' )
+	else:
+		file.write( '			roughness 0.8\n' )
 	for i in indices:
 		if len( options[i] ) > 0:
 			file.write( '			' + options[i] + '\n' )
 	for i in indices:
-		writeChannel( file, channels[i], extensions[i] )
+		writeChannel( file, channels[i], channelsMasks[i], extensions[i] )
 	file.write( '		}\n' )
 	file.write( '	}\n' )
 	file.write( '\n' )
-	file.write( '	scene_node "SunLightNode1"\n' )
+	file.write( '	scene_node "LightNode"\n' )
 	file.write( '	{\n' )
 	file.write( '		orientation 1 0 0 45\n' )
 	file.write( '	}\n' )
 	file.write( '	light "SunLight1"\n' )
 	file.write( '	{\n' )
-	file.write( '		parent "SunLightNode1"\n' )
+	file.write( '		parent "LightNode"\n' )
 	file.write( '		type directional\n' )
 	file.write( '		colour 1.0 1.0 1.0\n' )
 	file.write( '		intensity 0.8 1.0\n' )
@@ -76,20 +95,21 @@ def writeFile( file, indices ):
 	file.write( '	{\n' )
 	file.write( '		position 0.0 0.0 0.0\n' )
 	file.write( '	}\n' )
+	file.write( '	mesh "Mesh"\n' )
+	file.write( '	{\n' )
+	file.write( '		type "cube" -width=50 -height=50 depth=50\n' )
+	file.write( '		default_material "Textured"\n' )
+	file.write( '	}\n' )
 	file.write( '	object "FinalPrimitive"\n' )
 	file.write( '	{\n' )
 	file.write( '		parent "FinalNode"\n' )
 	file.write( '		mesh "Mesh"\n' )
-	file.write( '		{\n' )
-	file.write( '			type "cube" -width=50 -height=50 depth=50\n' )
-	file.write( '		}\n' )
-	file.write( '		material "Textured"\n' )
 	file.write( '	}\n' )
 	file.write( '\n' )
 	file.write( '	scene_node "MainCameraNode"\n' )
 	file.write( '	{\n' )
 	file.write( '		position 87.3926 -3.91655 -82.7833\n' )
-	file.write( '		orientation -0.0337855 -0.999314 -0.0151473 48.3264\n' )
+	file.write( '		orientation 0.0 1.0 0.0 315.0\n' )
 	file.write( '	}\n' )
 	file.write( '	camera "MainCamera"\n' )
 	file.write( '	{\n' )
@@ -100,14 +120,16 @@ def writeFile( file, indices ):
 	file.write( '			type perspective\n' )
 	file.write( '			fov_y 45.0\n' )
 	file.write( '			aspect_ratio 1.333\n' )
-	file.write( '			near 0.1\n' )
-	file.write( '			far 20000.0\n' )
+	file.write( '			near 1.0\n' )
+	file.write( '			far 2000.0\n' )
 	file.write( '		}\n' )
 	file.write( '	}\n' )
 	file.write( '}\n' )
 	file.write( '\n' )
 	file.write( 'window "Window"\n' )
 	file.write( '{\n' )
+	file.write( '	fullscreen false\n' )
+	file.write( '	vsync false\n' )
 	file.write( '	render_target\n' )
 	file.write( '	{\n' )
 	file.write( '		format argb32\n' )
@@ -115,15 +137,13 @@ def writeFile( file, indices ):
 	file.write( '		scene "Scene"\n' )
 	file.write( '		camera "MainCamera"\n' )
 	file.write( '	}\n' )
-	file.write( '	fullscreen false\n' )
-	file.write( '	vsync false\n' )
 	file.write( '}\n' )
 	file.write( '\n' )
 
-def generate1Channel( names, index, count, begin, end ):
+def generate1Channel( index, count, begin, end ):
 	global startIndex
 	for i in range( begin, end ):
-		file = open( "{:04}".format( startIndex ) + "-" + names + channels[i].capitalize() + ".cscn", "w" )
+		file = open( "Test-{:04}".format( startIndex ) + ".cscn", "w" )
 		startIndex = startIndex + 1
 		list = []
 		if index != -1:
@@ -132,94 +152,94 @@ def generate1Channel( names, index, count, begin, end ):
 		writeFile( file, list )
 		file.close()
 
-generate1Channel( "", -1, 1, 0, len( channels ) - 0 )
+generate1Channel( -1, 1, 0, len( channels ) - 0 )
 
-def generate2Channels( names, index, count, begin, end ):
+def generate2Channels( index, count, begin, end ):
 	for i in range( begin, end ):
 		list = []
 		if index != -1:
 			list = index[:]
 		list.append( i )
-		generate1Channel( names + channels[i].capitalize(), list, count, i + 1, end + 1 )
+		generate1Channel( list, count, i + 1, end + 1 )
 
-generate2Channels( "", -1, 2, 0, len( channels ) - 1 )
+generate2Channels( -1, 2, 0, len( channels ) - 1 )
 
-def generate3Channels( names, index, count, begin, end ):
+def generate3Channels( index, count, begin, end ):
 	for i in range( begin, end ):
 		list = []
 		if index != -1:
 			list = index[:]
 		list.append( i )
-		generate2Channels( names + channels[i].capitalize(), list, count, i + 1, end + 1 )
+		generate2Channels( list, count, i + 1, end + 1 )
 
-generate3Channels( "", -1, 3, 0, len( channels ) - 2 )
+generate3Channels( -1, 3, 0, len( channels ) - 2 )
 
-def generate4Channels( names, index, count, begin, end ):
+def generate4Channels( index, count, begin, end ):
 	for i in range( begin, end ):
 		list = []
 		if index != -1:
 			list = index[:]
 		list.append( i )
-		generate3Channels( names + channels[i].capitalize(), list, count, i + 1, end + 1 )
+		generate3Channels( list, count, i + 1, end + 1 )
 
-generate4Channels( "", -1, 4, 0, len( channels ) - 3 )
+generate4Channels( -1, 4, 0, len( channels ) - 3 )
 
-def generate5Channels( names, index, count, begin, end ):
+def generate5Channels( index, count, begin, end ):
 	for i in range( begin, end ):
 		list = []
 		if index != -1:
 			list = index[:]
 		list.append( i )
-		generate4Channels( names + channels[i].capitalize(), list, count, i + 1, end + 1 )
+		generate4Channels( list, count, i + 1, end + 1 )
 
-generate5Channels( "", -1, 5, 0, len( channels ) - 4 )
+generate5Channels( -1, 5, 0, len( channels ) - 4 )
 
-def generate6Channels( names, index, count, begin, end ):
+def generate6Channels( index, count, begin, end ):
 	for i in range( begin, end ):
 		list = []
 		if index != -1:
 			list = index[:]
 		list.append( i )
-		generate5Channels( names + channels[i].capitalize(), list, count, i + 1, end + 1 )
+		generate5Channels( list, count, i + 1, end + 1 )
 
-generate6Channels( "", -1, 6, 0, len( channels ) - 5 )
+generate6Channels( -1, 6, 0, len( channels ) - 5 )
 
-def generate7Channels( names, index, count, begin, end ):
+def generate7Channels( index, count, begin, end ):
 	for i in range( begin, end ):
 		list = []
 		if index != -1:
 			list = index[:]
 		list.append( i )
-		generate6Channels( names + channels[i].capitalize(), list, count, i + 1, end + 1 )
+		generate6Channels( list, count, i + 1, end + 1 )
 
-generate7Channels( "", -1, 7, 0, len( channels ) - 6 )
+generate7Channels( -1, 7, 0, len( channels ) - 6 )
 
-def generate8Channels( names, index, count, begin, end ):
+def generate8Channels( index, count, begin, end ):
 	for i in range( begin, end ):
 		list = []
 		if index != -1:
 			list = index[:]
 		list.append( i )
-		generate7Channels( names + channels[i].capitalize(), list, count, i + 1, end + 1 )
+		generate7Channels( list, count, i + 1, end + 1 )
 
-generate8Channels( "", -1, 8, 0, len( channels ) - 7 )
+generate8Channels( -1, 8, 0, len( channels ) - 7 )
 
-def generate9Channels( names, index, count, begin, end ):
+def generate9Channels( index, count, begin, end ):
 	for i in range( begin, end ):
 		list = []
 		if index != -1:
 			list = index[:]
 		list.append( i )
-		generate8Channels( names + channels[i].capitalize(), list, count, i + 1, end + 1 )
+		generate8Channels( list, count, i + 1, end + 1 )
 
-generate9Channels( "", -1, 9, 0, len( channels ) - 8 )
+generate9Channels( -1, 9, 0, len( channels ) - 8 )
 
-def generate10Channels( names, index, count, begin, end ):
+def generate10Channels( index, count, begin, end ):
 	for i in range( begin, end ):
 		list = []
 		if index != -1:
 			list = index[:]
 		list.append( i )
-		generate9Channels( names + channels[i].capitalize(), list, count, i + 1, end + 1 )
+		generate9Channels( list, count, i + 1, end + 1 )
 
-generate10Channels( "", -1, 10, 0, len( channels ) - 9 )
+generate10Channels( -1, 10, 0, len( channels ) - 9 )
