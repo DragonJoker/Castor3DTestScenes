717e48aaf3f3f00eb681e1e114e8a85e35107e37
Sylvain Doremus <dragonjoker59@hotmail.com>
2023-11-10 11:15:18 +0100

    Updated to latest development.

--- Common/Shaders/particleSystemCompute1M.glsl
+++ Common/Shaders/particleSystemCompute1M.glsl
@@ -1,10 +1,10 @@
 #version 430
 layout(local_size_x = 32, local_size_y = 32, local_size_z = 1)in;
 
-#define in_launcherCooldown 0.1
-#define in_shellLifetime 1.0
-#define in_secondaryShellLifetime 2.0
-#define in_finalShellLifetime 250000.0
+#define inLauncherCooldown 0.1
+#define inShellLifetime 1.0
+#define inSecondaryShellLifetime 2.0
+#define inFinalShellLifetime 250000.0
 
 #define PARTICLE_TYPE_LAUNCHER 0
 #define PARTICLE_TYPE_SHELL 1
@@ -13,151 +13,149 @@ layout(local_size_x = 32, local_size_y = 32, local_size_z = 1)in;
 
 struct Particle
 {
-	vec4 positionType;
-	vec4 velocityAge;
+	vec3 position;
+	int type;
+	vec3 velocity;
+	float age;
 };
 
-layout( std140, binding = 0 ) buffer Index
+layout( std430, binding = 0 ) buffer C3D_Index
 {
-	uint out_index;
-	uint in_index;
+	uint c3d_outIndex;
+	uint c3d_inIndex;
 };
 
-layout( std140, binding = 1 ) buffer Random
+layout( std430, binding = 1 ) buffer C3D_Random
 {
-	vec4 random[1024];
+	vec4 c3d_random[1024];
 };
 
-layout( std140, binding = 2 ) buffer ParticlesIn
+layout( std430, binding = 2 ) buffer C3D_ParticlesIn
 {
-	Particle particles_in[];
+	Particle c3d_inParticles[];
 };
 
-layout( std140, binding = 3 ) buffer ParticlesOut
+layout( std430, binding = 3 ) buffer C3D_ParticlesOut
 {
-	Particle particles_out[];
+	Particle c3d_outParticles[];
 };
 
-layout( std140, binding = 4 ) uniform ParticleSystem
+layout( std140, binding = 4 ) uniform C3D_ParticleSystem
 {
-	float c3d_fDeltaTime;
-	float c3d_fTotalTime;
-	uint c3d_uiMaxParticlesCount;
-	uint c3d_uiCurrentParticlesCount;
-	vec3 c3d_v3EmitterPosition;
+	float c3d_deltaTime;
+	float c3d_totalTime;
+	uint c3d_maxParticlesCount;
+	uint c3d_currentParticlesCount;
+	vec3 c3d_emitterPosition;
 };
 
-vec3 GetRandomDir( float p_uv )
+vec3 GetRandomDir( float uv )
 {
-	uint l_uv = uint( mod( p_uv, 1024.0 ) );
-	return random[l_uv].xyz;
+	uint index = uint( mod( uv, 1024.0 ) );
+	return c3d_random[index].xyz;
 }
 
-void EmitParticle( in float p_type, in vec3 p_position, in vec3 p_velocity, in float p_age )
+void EmitParticle( in int type, in vec3 position, in vec3 velocity, in float age )
 {
-	uint l_index = atomicAdd( out_index, 1u );
-	particles_out[l_index].positionType.w = p_type;
-	particles_out[l_index].positionType.xyz = p_position;
-	particles_out[l_index].velocityAge.xyz = p_velocity;
-	particles_out[l_index].velocityAge.w = p_age;
+	uint index = atomicAdd( c3d_outIndex, 1u );
+
+	if ( index < c3d_maxParticlesCount )
+	{
+		c3d_outParticles[index].position = position;
+		c3d_outParticles[index].type = type;
+		c3d_outParticles[index].velocity = velocity;
+		c3d_outParticles[index].age = age;
+	}
 }
 
 void main()
 {
-	uint l_gid = atomicAdd( in_index, 1u );
+	uint gid = atomicAdd( c3d_inIndex, 1u );
 
-	if ( l_gid < c3d_uiCurrentParticlesCount )
+	if ( gid < c3d_currentParticlesCount )
 	{
-		Particle l_in = particles_in[l_gid];
-		float l_age = l_in.velocityAge.w + c3d_fDeltaTime;
-		int l_type = int( l_in.positionType.w );
-		vec3 l_position = l_in.positionType.xyz;
-		vec3 l_velocity = l_in.velocityAge.xyz;
+		Particle inParticle = c3d_inParticles[gid];
+		float age = inParticle.age + c3d_deltaTime;
+		int type = inParticle.type;
+		vec3 position = inParticle.position;
+		vec3 velocity = inParticle.velocity;
 
-		switch ( l_type )
+		switch ( type )
 		{
 		case PARTICLE_TYPE_LAUNCHER:
-			if ( l_age >= in_launcherCooldown )
+			if ( age >= inLauncherCooldown )
 			{
-				l_age = 0.0;
-				vec3 l_dir = GetRandomDir( c3d_fTotalTime ) * 5.0;
-				l_dir.y = max( l_dir.y * 7.0, 30.0 );
-				EmitParticle(
-					PARTICLE_TYPE_SHELL,
-					l_position,
-					l_dir,
-					l_age );
+				age = 0.0;
+				vec3 dir = GetRandomDir( c3d_totalTime ) * 25.0;
+				EmitParticle( PARTICLE_TYPE_SHELL
+					, position
+					, dir
+					, age );
 			}
-			EmitParticle(
-				PARTICLE_TYPE_LAUNCHER,
-				c3d_v3EmitterPosition,
-				l_velocity,
-				l_age );
+			EmitParticle( PARTICLE_TYPE_LAUNCHER
+				, c3d_emitterPosition
+				, velocity
+				, age );
 			break;
 
 		case PARTICLE_TYPE_SHELL:
-			if ( l_age < in_shellLifetime )
+			if ( age < inShellLifetime )
 			{
-				float l_deltaTime = c3d_fDeltaTime / 1000.0;
-				vec3 l_deltaP = vec3( l_deltaTime ) * l_velocity;
-				vec3 l_deltaV = vec3( l_deltaTime ) * vec3( 0.0, -0.981, 0.0 );
-				EmitParticle(
-					PARTICLE_TYPE_SHELL,
-					l_position + l_deltaP,
-					l_velocity + l_deltaV,
-					l_age );
+				float deltaTime = c3d_deltaTime / 1000.0;
+				vec3 deltaP = vec3( deltaTime ) * velocity;
+				vec3 deltaV = vec3( deltaTime ) * vec3( -0.2, -0.2, -0.2 );
+				EmitParticle( PARTICLE_TYPE_SHELL
+					, position + deltaP
+					, velocity + deltaV
+					, age );
 			}
 			else
 			{
 				for ( int i = 0; i < 10; i++ )
 				{
-					vec3 l_dir = GetRandomDir( c3d_fTotalTime + i ) * 5.0;
-					EmitParticle(
-						PARTICLE_TYPE_SECONDARY_SHELL,
-						l_position,
-						l_dir + l_velocity / 2,
-						0.0 );
+					vec3 dir = GetRandomDir( c3d_totalTime + i ) * 5.0;
+					EmitParticle( PARTICLE_TYPE_SECONDARY_SHELL
+						, position
+						, dir + velocity / 2
+						, 0.0 );
 				}
 			}
 			break;
 
 		case PARTICLE_TYPE_SECONDARY_SHELL:
-			if ( l_age < in_secondaryShellLifetime )
+			if ( age < inSecondaryShellLifetime )
 			{
-				float l_deltaTime = c3d_fDeltaTime / 1000.0;
-				vec3 l_deltaP = vec3( l_deltaTime ) * l_velocity;
-				vec3 l_deltaV = vec3( l_deltaTime ) * vec3( 0.0, -0.981, 0.0 );
-				EmitParticle(
-					PARTICLE_TYPE_SECONDARY_SHELL,
-					l_position + l_deltaP,
-					l_velocity + l_deltaV,
-					l_age );
+				float deltaTime = c3d_deltaTime / 1000.0;
+				vec3 deltaP = vec3( deltaTime ) * velocity;
+				vec3 deltaV = vec3( deltaTime ) * vec3( -0.2, -0.2, -0.2 );
+				EmitParticle( PARTICLE_TYPE_SECONDARY_SHELL
+					, position + deltaP
+					, velocity + deltaV
+					, age );
 			}
 			else
 			{
 				for ( int i = 0; i < 10; i++ )
 				{
-					vec3 l_dir = GetRandomDir( c3d_fTotalTime + i ) * 5.0;
-					EmitParticle(
-						PARTICLE_TYPE_FINAL_SHELL,
-						l_position,
-						l_dir + l_velocity / 2,
-						0.0 );
+					vec3 dir = GetRandomDir( c3d_totalTime + i ) * 5.0;
+					EmitParticle( PARTICLE_TYPE_FINAL_SHELL
+						, position
+						, dir + velocity / 2
+						, 0.0 );
 				}
 			}
 			break;
 
 		case PARTICLE_TYPE_FINAL_SHELL:
-			if ( l_age < in_finalShellLifetime )
+			if ( age < inFinalShellLifetime )
 			{
-				float l_deltaTime = c3d_fDeltaTime / 1000.0;
-				vec3 l_deltaP = vec3( l_deltaTime ) * l_velocity;
-				vec3 l_deltaV = vec3( l_deltaTime ) * vec3( 0.0, -0.981, 0.0 );
-				EmitParticle(
-					PARTICLE_TYPE_FINAL_SHELL,
-					l_position + l_deltaP,
-					l_velocity + l_deltaV,
-					l_age );
+				float deltaTime = c3d_deltaTime / 1000.0;
+				vec3 deltaP = vec3( deltaTime ) * velocity;
+				vec3 deltaV = vec3( deltaTime ) * vec3( -0.2, -0.2, -0.2 );
+				EmitParticle( PARTICLE_TYPE_FINAL_SHELL
+					, position + deltaP
+					, velocity + deltaV
+					, age );
 			}
 			break;
 		}
